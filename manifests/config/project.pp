384bca02cb4baa68f6d9989b06c0079cc8f76fcb
Maximilian Gaß <m.gass@babiel.com>
2024-06-17 15:28:11 +0200

    fix: fix deprecation warning regarding create_ini_settings

--- manifests/config/project.pp
+++ manifests/config/project.pp
@@ -180,5 +180,5 @@ define rundeck::config::project (
     require => File[$properties_file],
   }
 
-  create_ini_settings($node_executor_settings, $node_executor_settings_defaults)
+  inifile::create_ini_settings($node_executor_settings, $node_executor_settings_defaults)
 }
